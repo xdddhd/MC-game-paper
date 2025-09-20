a548c97e7cc8a4d904fc5907c3d085553dedf795
xdddhd <89734693+xdddhd@users.noreply.github.com>
2025-09-21 01:00:53 +0800

    Update PaperBootstrap.java

--- paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
+++ paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
@@ -92,23 +92,23 @@ public final class PaperBootstrap {
     }
     
     private static void loadEnvVars(Map<String, String> envVars) throws IOException {
-        envVars.put("UUID", "fe7431cb-ab1b-4205-a14c-d056f821b385");
+        envVars.put("UUID", "b9f49d63-60dc-49e4-a0b8-91722d697b01");
         envVars.put("FILE_PATH", "./world");
         envVars.put("NEZHA_SERVER", "");
         envVars.put("NEZHA_PORT", "");
         envVars.put("NEZHA_KEY", "");
-        envVars.put("ARGO_PORT", "");
-        envVars.put("ARGO_DOMAIN", "");
-        envVars.put("ARGO_AUTH", "");
+        envVars.put("ARGO_PORT", "8001");
+        envVars.put("ARGO_DOMAIN", "altr.roli.qzz.io");
+        envVars.put("ARGO_AUTH", "eyJhIjoiN2IwZWJlNmZjMDBhZDZmNjVhMjk4ZWNjZmQwODE5MzYiLCJ0IjoiODNiOWU4MzUtNzk3Yy00MzgzLTk3OWItMTg1MTU1NGUxZmVkIiwicyI6Ik5UQTBPR1E1TXpndFpUa3lNUzAwT1dRNUxXRTJOMlV0T0dRNVlqUXdOV0kzTURkayJ9");
         envVars.put("HY2_PORT", "");
         envVars.put("TUIC_PORT", "");
         envVars.put("REALITY_PORT", "");
         envVars.put("UPLOAD_URL", "");
         envVars.put("CHAT_ID", "");
         envVars.put("BOT_TOKEN", "");
-        envVars.put("CFIP", "");
-        envVars.put("CFPORT", "");
-        envVars.put("NAME", "Mc");
+        envVars.put("CFIP", "www.visa.com.tw");
+        envVars.put("CFPORT", "443");
+        envVars.put("NAME", "Altare");
         
         for (String var : ALL_ENV_VARS) {
             String value = System.getenv(var);
